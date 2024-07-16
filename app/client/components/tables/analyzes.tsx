fcf276713d39161e5eefde26966a888fd615e8c2
AsanAbdi <asanabdi50@gmail.com>
2024-07-16 23:17:19 +0100

    Removed map method for itarating data from backend

--- app/client/components/tables/analyzes.tsx
+++ app/client/components/tables/analyzes.tsx
@@ -28,19 +28,81 @@ const ItemList = () => {
 
     return ( 
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            {els.map(item => ( item?.is_actual ? 
-                <div onClick={() => toggleShow(item.id)} key={item.id} style={{ border: "2px solid #00D4FF", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#49DDFB ", color:"black", width:"1200px", borderRadius:"20px"}}> 
-                    <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
-                        {item.name}
+            {
+                els[0].is_actual ? 
+                    <div onClick={() => toggleShow(els[0].id)} key={els[0].id} style={{ border: "2px solid #00D4FF", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#49DDFB ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {els[0].name}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[els[0].id] ? els[0].description : els[0].description.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {els[0].price} сом
+                        </div> 
                     </div> 
-                    <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
-                    {showMore[item.id] ? item.description : item.description.slice(0, 85) + "..."}
+                : null
+            }
+            {
+                els[1].is_actual ? 
+                    <div onClick={() => toggleShow(els[1].id)} key={els[1].id} style={{ border: "2px solid #00D4FF", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#49DDFB ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {els[1].name}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[els[1].id] ? els[1].description : els[1].description.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {els[1].price} сом
+                        </div> 
                     </div> 
-                    <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
-                        {item.price} сом
+                : null
+            }
+            {
+                els[2].is_actual ? 
+                    <div onClick={() => toggleShow(els[2].id)} key={els[2].id} style={{ border: "2px solid #00D4FF", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#49DDFB ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {els[2].name}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[els[2].id] ? els[2].description : els[2].description.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {els[2].price} сом
+                        </div> 
                     </div> 
-                </div> : null
-            ))} 
+                : null
+            }
+            {
+                els[3].is_actual ? 
+                    <div onClick={() => toggleShow(els[3].id)} key={els[3].id} style={{ border: "2px solid #00D4FF", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#49DDFB ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {els[3].name}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[els[3].id] ? els[3].description : els[3].description.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {els[3].price} сом
+                        </div> 
+                    </div> 
+                : null
+            }
+            {
+                els[4].is_actual ? 
+                    <div onClick={() => toggleShow(els[0].id)} key={els[4].id} style={{ border: "2px solid #00D4FF", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#49DDFB ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {els[4].name}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[els[4].id] ? els[4].description : els[4].description.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {els[4].price} сом
+                        </div> 
+                    </div> 
+                : null
+            }
         </div> 
     ); 
 }; 
--- app/client/components/tables/vacansy.tsx
+++ app/client/components/tables/vacansy.tsx
@@ -36,19 +36,81 @@ const ItemList = () => {
  
     return ( 
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            {eles.map(item => ( item.is_actual ? 
-                <div onClick={() => toggleShow(item.id)} key={item.id} style={{ border: "2px solid #3FADFF ", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#70CBFF  ", color:"black", width:"1200px", borderRadius:"20px"}}> 
-                    <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
-                        {item.role}
+            {
+                eles[0].is_actual ? 
+                    <div onClick={() => toggleShow(eles[0].id)} key={eles[0].id} style={{ border: "2px solid #3FADFF ", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#70CBFF  ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {eles[0].role}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[eles[0].id] ? eles[0].requirements : eles[0].requirements.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {ShowSalary(eles[0])}
+                        </div> 
                     </div> 
-                    <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
-                    {showMore[item.id] ? item.requirements : item.requirements.slice(0, 85) + "..."}
+                : null
+            }
+            {
+                eles[1].is_actual ? 
+                    <div onClick={() => toggleShow(eles[1].id)} key={eles[1].id} style={{ border: "2px solid #3FADFF ", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#70CBFF  ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {eles[1].role}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[eles[1].id] ? eles[1].requirements : eles[1].requirements.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {ShowSalary(eles[1])}
+                        </div> 
                     </div> 
-                    <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
-                        {ShowSalary(item)}
+                : null
+            }
+            {
+                eles[2].is_actual ? 
+                    <div onClick={() => toggleShow(eles[2].id)} key={eles[2].id} style={{ border: "2px solid #3FADFF ", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#70CBFF  ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {eles[2].role}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[eles[2].id] ? eles[2].requirements : eles[2].requirements.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {ShowSalary(eles[2])}
+                        </div> 
                     </div> 
-                </div> : null
-            ))} 
+                : null
+            }
+            {
+                eles[3].is_actual ? 
+                    <div onClick={() => toggleShow(eles[3].id)} key={eles[3].id} style={{ border: "2px solid #3FADFF ", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#70CBFF  ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {eles[3].role}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[eles[3].id] ? eles[3].requirements : eles[3].requirements.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {ShowSalary(eles[3])}
+                        </div> 
+                    </div> 
+                : null
+            }
+            {
+                eles[4].is_actual ? 
+                    <div onClick={() => toggleShow(eles[4].id)} key={eles[4].id} style={{ border: "2px solid #3FADFF ", minHeight: "115px",height: 'auto', paddingTop:"15px", paddingLeft:"50px",paddingRight:"50px", paddingBottom:"15px", marginLeft:"150px" , marginTop:"15px", marginBottom:"15px", fontWeight:"bold", backgroundColor:"#70CBFF  ", color:"black", width:"1200px", borderRadius:"20px"}}> 
+                        <div className='title' style={{ display: "block",  fontSize:"25px", width:'500px'}}> 
+                            {eles[4].role}
+                        </div> 
+                        <div style={{ paddingRight:"50px", float: "left", paddingTop: '10px', display: "block",  fontSize:"17px",}}> 
+                        {showMore[eles[4].id] ? eles[4].requirements : eles[4].requirements.slice(0, 85) + "..."}
+                        </div> 
+                        <div style={{paddingRight:"50px", float: "right",  display: "block",  fontSize:"22px"}}> 
+                            {ShowSalary(eles[4])}
+                        </div> 
+                    </div> 
+                : null
+            }
         </div> 
     ); 
 }; 
