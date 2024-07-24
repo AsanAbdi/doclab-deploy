bbe28501bd508a503ea75780eeb844f31321e3e4
AsanAbdi <asanabdi50@gmail.com>
2024-07-24 04:08:15 +0100

    added adaptive styles

--- app/client/components/Banner/index.tsx
+++ app/client/components/Banner/index.tsx
@@ -1,37 +1,34 @@
-import Image from "next/image";
-
-const Banner = () => {
-    return (
-        <div className='mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8'>
-            <div className='grid grid-cols-1 lg:grid-cols-2 my-16 gap-10'>
-
-                {/* COLUMN-1 */}
-
-                <div className="mx-auto sm:mx-0">
-                    <div className='py-3 text-center lg:text-start hidden sm:none'>
-                        <button className='text-blue bg-lightblue hover:shadow-xl text-sm md:text-lg font-bold px-6 py-1 rounded-3xl tracking-wider hover:text-white hover:bg-black'>DESIGN AGENCY</button>
-                    </div>
-                    <div className="py-3 text-center lg:text-start">
-                        <h1 className='text-6xl lg:text50xl font-bold text-darkpurple'>
-                        Профессиональный уход о вашем здоровьем!<br /> <br /> <p className='text-4xl lg:text-60xl font-bold text-darkpurple'>Добро пожаловать в нашу клинику. <br /> Для нас ваше благополучие - наш главный приоритет. </p>
-                        </h1>
-                    </div>
-                    {/* <div className='my-7 text-center lg:text-start'>
-                        <button className='text-sm md:text-xl font-semibold hover:shadow-xl bg-blue text-white py-3 px-6 md:py-5 md:px-14 rounded-full hover:bg-hoblue'>
-                            Начать
-                        </button>
-                    </div> */}
-                </div>
-
-                {/* COLUMN-2 */}
-
-                <div className='lg:-m-24 lg:pt-20 hidden lg:block'>
-                    <Image src='/images/bunDoc.png' alt="hero-image" width={800} height={642} />
-                </div>
-
-            </div>
-        </div>
-    )
-}
-
+import Image from "next/image"; 
+ 
+const Banner = () => { 
+    return ( 
+        <div className='mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8'> 
+            <div className='flex flex-col lg:flex-row items-center gap-10'> 
+                 
+                {/* COLUMN-1 */} 
+                 
+                <div className="flex flex-col justify-center text-center lg:text-start lg:w-1/2"> 
+                    <div className="py-3"> 
+                        <h1 className='text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-darkpurple'> 
+                            Профессиональный уход за вашим здоровьем! 
+                        </h1> 
+                    </div> 
+                    <div className='text-center lg:text-start'> 
+                        <p className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-darkpurple'> 
+                            Добро пожаловать в нашу клинику. <br /> Для нас ваше благополучие - наш главный приоритет. 
+                        </p> 
+                    </div> 
+                </div> 
+ 
+                {/* IMAGE */} 
+                 
+                <div className='flex justify-center items-center lg:w-1/2'> 
+                    <Image src='/images/bunDoc.png' alt="hero-image" width={800} height={642} layout='responsive' /> 
+                </div> 
+ 
+            </div> 
+        </div> 
+    ) 
+} 
+ 
 export default Banner;
--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,37 +1,38 @@
-import Banner from './components/Banner/index';
-import Aboutus from './components/Aboutus/index';
-import Dedicated from './components/Dedicated/index';
-import Digital from './components/Digital/index';
-import Beliefs from './components/Beliefs/index';
-import Wework from './components/Wework/index';
-import Ourteam from './components/Ourteam/index';
-import Featured from './components/Featured/index';
-import Manage from './components/Manage/index';
-import FAQ from './components/FAQ/index';
-import Testimonials from './components/Testimonials/index';
-import Articles from './components/Articles/index';
-import Joinus from './components/Joinus/index';
-import Insta from './components/Insta/index';
-
-
-export default function Home() {
-  return (
-    <main>
-      <Banner />
-      {/* <Aboutus /> */}
-      <Digital />
-      <Dedicated />
-      {/* <Beliefs /> */}
-      {/* <Wework /> */}
-      <Ourteam />
-      {/* <Featured /> */}
-      {/* <Manage />  */}
-      {/* <FAQ /> */}
-      {/* <Testimonials /> */}
-      {/* <Articles /> */}
-      {/* <Joinus /> */}
-      {/* <Insta /> */}                                                                               
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3152.378908227124!2d13.035541632349121!3d77.60936996196725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDQyJzA3LjciTiA1wrAyOCcwMy41Ilc!5e0!3m2!1sen!2sus!4v1618298271234!5m2!1sen!2sus" width="600" height="450" style={{width: '1000px', height: '400px', margin: '60px auto'}} loading="lazy"></iframe>
-    </main>
-  )
+import Banner from './components/Banner/index'; 
+import Aboutus from './components/Aboutus/index'; 
+import Dedicated from './components/Dedicated/index'; 
+import Digital from './components/Digital/index'; 
+import Beliefs from './components/Beliefs/index'; 
+import Wework from './components/Wework/index'; 
+import Ourteam from './components/Ourteam/index'; 
+import Featured from './components/Featured/index'; 
+import Manage from './components/Manage/index'; 
+import FAQ from './components/FAQ/index'; 
+import Testimonials from './components/Testimonials/index'; 
+import Articles from './components/Articles/index'; 
+import Joinus from './components/Joinus/index'; 
+import Insta from './components/Insta/index'; 
+import Map from './components/Map/index'; 
+ 
+ 
+export default function Home() { 
+  return ( 
+    <main> 
+      <Banner /> 
+      {/* <Aboutus /> */} 
+      <Digital /> 
+      <Dedicated /> 
+      {/* <Beliefs /> */} 
+      {/* <Wework /> */} 
+      <Ourteam /> 
+      {/* <Featured /> */} 
+      {/* <Manage />  */} 
+      {/* <FAQ /> */} 
+      {/* <Testimonials /> */} 
+      {/* <Articles /> */} 
+      {/* <Joinus /> */} 
+      {/* <Insta /> */}                                                                                
+      <Map /> 
+          </main> 
+  ) 
 }
