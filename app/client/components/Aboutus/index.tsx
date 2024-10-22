47e77d98cecb6a86f5c8aead443d1858dfb0aa36
AlmazAlan <alymbekalmazbekov0@gmail.com>
2024-10-23 02:47:07 +0600

    navbar, banners etc

--- app/client/components/Aboutus/index.tsx
+++ app/client/components/Aboutus/index.tsx
@@ -1,82 +1,62 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
-
-interface datatype {
-    heading: string;
-    imgSrc: string;
-    paragraph: string;
-    link: string;
-}
-
-const Aboutdata: datatype[] = [
-    {
-        heading: "Модернизация",
-        imgSrc: "/images/aboutus/imgOne.svg",
-        paragraph: "Do tempor non commodo culpa incididunt eu ex sit. Excepteur dolore sint sunt aute aliqua velit tempor duis officia commodo enim pariatur.",
-        link: 'Узнать больше'
-    },
-    {
-        heading: "Повышение квалификации",
-        imgSrc: "/images/aboutus/imgTwo.svg",
-        paragraph: 'Est Lorem pariatur dolor elit occaecat qui do sint consequat commodo ut.Consectetur officia incididunt Lorem est nostrud pariatur officia non.',
-        link: 'Узнать больше'
-    },
-    {
-        heading: "Сотрудничество",
-        imgSrc: "/images/aboutus/imgThree.svg",
-        paragraph: 'Est commodo est anim sint Lorem in magna officia enim amet aliqua aute.Nulla qui Lorem sint non dolore nulla excepteur est proident eu mollit dolor laborum culpa.',
-        link: 'Узнать больше'
-    },
-]
+import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 const Aboutus = () => {
-    return (
+  return (
+    <div id="aboutus-section">
+      <div className="mx-auto max-w-5xl px-4 py-16 my-24 lg:px-8 bg-lightgrey rounded-3xl relative">
+        {/* Заголовок по центру, больший размер и меньше отступов */}
+        <h2 className="text-center text-5xl font-bold text-black mb-12">DocLab</h2>
+        
+        {/* Декоративное изображение */}
+        <Image
+          src="/images/aboutus/dots.svg"
+          width={100}
+          height={100}
+          alt="dots-image"
+          className="absolute bottom-1 -left-20"
+        />
 
-        <div id="aboutus-section">
-            <div className='mx-auto max-w-7xl px-4 py-20 my-32 lg:px-10 bg-lightgrey rounded-3xl relative'>
-                <Image src="/images/aboutus/dots.svg" width={100} height={100} alt="dots-image" className="absolute bottom-1 -left-20" />
-                {/* <h3 className='text-center text-blue text-lg tracking-widest'>О НАС</h3> */}
-                {/* <h4 className='text-center text-4xl lg:text-65xl font-bold'>Узнайте больше о нашей деятельности.</h4> */}
-                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-16 gap-x-16 lg:gap-x-32'>
-                    <div className='hover:bg-navyblue bg-white rounded-3xl mt-16 pt-10 pl-8 pb-10 pr-6 shadow-xl group'>
-                        <h4 className='text-2xl font-semibold  text-black mb-5 group-hover:text-white'>DocLab</h4>
-                        {/* <Image src="/images/aboutus/imgOne.svg" alt="/images/aboutus/imgOne.svg" width={100} height={100} className="mb-5" /> */}
-                        <h4 className='text-lg font-normal text-black group-hover:text-offwhite mb-5'>
-                            Узнайте, как модернизация лабораторий в Центральной Азии поднимет научные исследования на новый уровень, улучшит методы работы и приведет к передовым результатам. Подробнее здесь!
-                        </h4>
-                        <Link href="/client/section1/" className='text-lg font-semibold group-hover:text-white text-blue hover-underline'>
-                            Узнать больше...
-                            <ChevronRightIcon width={20} height={20} />
-                        </Link>
-                    </div>
-                    <div className='hover:bg-navyblue bg-white rounded-3xl mt-16 pt-10 pl-8 pb-10 pr-6 shadow-xl group'>
-                        <h4 className='text-2xl font-semibold  text-black mb-5 group-hover:text-white'>Digital pathology</h4>
-                        {/* <Image src="/images/aboutus/imgTwo.svg" alt="/images/aboutus/imgTwo.svg" width={100} height={100} className="mb-5" /> */}
-                        <h4 className='text-lg font-normal text-black group-hover:text-offwhite mb-5'>
-                            Откройте для себя, как программы профессионального развития в лабораториях Центральной Азии улучшают навыки сотрудников и повышают эффективность работы. Подробнее здесь!
-                        </h4>
-                        <Link href="/client/section2/" className='text-lg font-semibold group-hover:text-white text-blue hover-underline'>
-                            Узнать больше...
-                            <ChevronRightIcon width={20} height={20} />
-                        </Link>
-                    </div>
-                    <div className='hover:bg-navyblue bg-white rounded-3xl mt-16 pt-10 pl-8 pb-10 pr-6 shadow-xl group'>
-                        <h4 className='text-2xl font-semibold  text-black mb-5 group-hover:text-white'>Оборудование</h4>
-                        {/* <Image src="/images/aboutus/imgThree.svg" alt="/images/aboutus/imgThree.svg" width={100} height={100} className="mb-5" /> */}
-                        <h4 className='text-lg font-normal text-black group-hover:text-offwhite mb-5'>
-                            Откройте, как сотрудничество между лабораториями в Центральной Азии способствует обмену знаниями, укреплению научных связей и развитию совместных проектов в области науки и технологий. Подробнее на странице!
-                        </h4>
-                        <Link href="/client/section3/" className='text-lg font-semibold group-hover:text-white text-blue hover-underline'>
-                            Узнать больше...
-                            <ChevronRightIcon width={20} height={20} />
-                        </Link>
-                    </div>
-                </div>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-3xl">
+            {/* Первый блок */}
+            <div className="hover:bg-navyblue bg-white rounded-3xl p-6 shadow-xl group max-w-xs mx-auto">
+              <h4 className="text-2xl font-semibold text-black mb-5 group-hover:text-white">DocLab</h4>
+              <h4 className="text-lg font-normal text-black group-hover:text-offwhite mb-5">
+                Миссия <b>DocLab Pathology</b> — высокая точность диагностики в соответствии с международными стандартами и
+                протоколами. Мы следуем всем необходимым нормам и правилам, обеспечивая высокое
+              </h4>
+              <Link
+                href="/client/section1/"
+                className="text-lg font-semibold group-hover:text-white text-blue hover-underline"
+              >
+                Узнать больше...
+                <ChevronRightIcon width={20} height={20} />
+              </Link>
             </div>
-        </div>
 
-    )
-}
+            {/* Второй блок */}
+            <div className="hover:bg-navyblue bg-white rounded-3xl p-6 shadow-xl group max-w-xs mx-auto">
+              <h4 className="text-2xl font-semibold text-black mb-5 group-hover:text-white">Digital pathology</h4>
+              <h4 className="text-lg font-normal text-black group-hover:text-offwhite mb-5">
+                Цифровая патология (<b>Digital Pathology</b>) — это область патологии, которая использует цифровые
+                технологии для анализа образцов тканей и клеток. Вместо традиционных методов исследования
+              </h4>
+              <Link
+                href="/client/section2/"
+                className="text-lg font-semibold group-hover:text-white text-blue hover-underline"
+              >
+                Узнать больше...
+                <ChevronRightIcon width={20} height={20} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Aboutus;
---
+++ app/client/components/LabDocs/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"; 
+
+const LabDocs = () => {
+    return (
+      <div className='flex justify-center items-center lg:w-1/2'> 
+                        <Image src='/images/bunDoc.png' alt="hero-image" width={800} height={642} layout='responsive' /> 
+                    </div> 
+    );
+  };
+  
+  export default LabDocs;
+  
